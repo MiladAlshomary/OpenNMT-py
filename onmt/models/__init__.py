902de17f83c7970626bca5d60f70f9daba763ec7
Milad <milad.alshomary@uni-weimar.de>
2019-09-27 13:03:16 +0200

    updates

--- onmt/models/__init__.py
+++ onmt/models/__init__.py
@@ -1,8 +1,8 @@
 """Module defining models."""
 from onmt.models.model_saver import build_model_saver, ModelSaver
 from onmt.models.model import NMTModel
-from onmt.models.model import ContextualFeaturesProjector
+from onmt.models.model import ContextualFeaturesProjector, ContextLocalFeaturesProjector
 from onmt.models.model import NMTContextDModel
 
 __all__ = ["build_model_saver", "ModelSaver",
-           "NMTModel", "ContextualFeaturesProjector", "NMTContextDModel", "check_sru_requirement"]
+           "NMTModel", "ContextualFeaturesProjector", "ContextLocalFeaturesProjector", "NMTContextDModel", "check_sru_requirement"]
