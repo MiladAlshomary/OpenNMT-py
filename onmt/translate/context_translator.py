c0e40fc50e67de08dee553253f8ffc5c3893585d
Milad <milad.alshomary@uni-weimar.de>
2019-10-01 15:40:01 +0200

    updates

--- onmt/translate/context_translator.py
+++ onmt/translate/context_translator.py
@@ -882,3 +882,232 @@ class ContextTranslator(object):
             shell=True, stdin=self.out_file
         ).decode("utf-8").strip()
         return msg
+
+
+def ContextTranslatorViaAttn(ContextTranslator):
+
+    def __init__(
+            self,
+            model,
+            fields,
+            src_reader,
+            tgt_reader,
+            gpu=-1,
+            n_best=1,
+            min_length=0,
+            max_length=100,
+            ratio=0.,
+            beam_size=30,
+            random_sampling_topk=1,
+            random_sampling_temp=1,
+            stepwise_penalty=None,
+            dump_beam=False,
+            block_ngram_repeat=0,
+            ignore_when_blocking=frozenset(),
+            replace_unk=False,
+            constraint_file=None,
+            phrase_table="",
+            data_type="text",
+            verbose=False,
+            report_bleu=False,
+            report_rouge=False,
+            report_time=False,
+            copy_attn=False,
+            global_scorer=None,
+            out_file=None,
+            report_score=True,
+            logger=None,
+            seed=-1):
+
+        super(ContextTranslatorViaAttn, self).__init__(model, fields, src_reader, tgt_reader, gpu, n_best, min_length, max_length,
+            ratio, beam_size, random_sampling_topk, random_sampling_temp, stepwise_penalty, dump_beam, block_ngram_repeat, ignore_when_blocking,
+            replace_unk, constraint_file, phrase_table, data_type, verbose, report_bleu, report_rouge, report_time, copy_attn,
+            global_scorer, out_file, report_score, logger, seed)
+
+
+    def _translate_batch(
+            self,
+            batch,
+            context_feats,
+            src_vocabs,
+            max_length,
+            min_length=0,
+            ratio=0.,
+            n_best=1,
+            return_attention=False, tags=[]):
+        # TODO: support these blacklisted features.
+        assert not self.dump_beam
+        assert not self.copy_attn
+
+        # (0) Prep the components of the search.
+        use_src_map = self.copy_attn
+        beam_size = self.beam_size
+        batch_size = batch.batch_size
+
+        # Encode context features...
+        idxs  = batch.indices.cpu().data.numpy()
+        context_feats = torch.from_numpy( context_feats[idxs] )
+        context_feats = torch.autograd.Variable(context_feats, requires_grad=False)
+        if next(self.model.parameters()).is_cuda:
+            context_feats = context_feats.cuda()
+        else:
+            context_feats = context_feats.cpu()
+
+        #add dummy one dimension..
+        context_feats = context_feats.unsqueeze(-1)
+        # project image features
+        feats_proj = self.model.context_encoder( context_feats )
+
+        # (1) Run the encoder on the src.
+        src, enc_states, memory_bank, src_lengths = self._run_encoder(batch)
+
+    
+        # initialise decoder
+        self.model.decoder.init_state(memory_bank, feats_proj, enc_init_state)
+
+        results = {
+            "predictions": None,
+            "scores": None,
+            "attention": None,
+            "batch": batch,
+            "gold_score": self._gold_score(
+                batch, memory_bank, src_lengths, src_vocabs, use_src_map,
+                enc_states, batch_size, src)}
+
+        # (2) Repeat src objects `beam_size` times.
+        # We use batch_size x beam_size
+        src_map = (tile(batch.src_map, beam_size, dim=1)
+                   if use_src_map else None)
+        self.model.decoder.map_state(
+            lambda state, dim: tile(state, beam_size, dim=dim))
+
+        if isinstance(memory_bank, tuple):
+            memory_bank = tuple(tile(x, beam_size, dim=1) for x in memory_bank)
+            mb_device = memory_bank[0].device
+        else:
+            memory_bank = tile(memory_bank, beam_size, dim=1)
+            mb_device = memory_bank.device
+        memory_lengths = tile(src_lengths, beam_size)
+
+        # (0) pt 2, prep the beam object
+        beam = BeamSearch(
+            beam_size,
+            n_best=n_best,
+            batch_size=batch_size,
+            global_scorer=self.global_scorer,
+            pad=self._tgt_pad_idx,
+            eos=self._tgt_eos_idx,
+            bos=self._tgt_bos_idx,
+            min_length=min_length,
+            ratio=ratio,
+            max_length=max_length,
+            mb_device=mb_device,
+            return_attention=return_attention,
+            stepwise_penalty=self.stepwise_penalty,
+            block_ngram_repeat=self.block_ngram_repeat,
+            exclusion_tokens=self._exclusion_idxs,
+            memory_lengths=memory_lengths)
+
+        for step in range(max_length):
+            decoder_input = beam.current_predictions.view(1, -1, 1)
+
+            log_probs, attn = self._decode_and_generate(
+                decoder_input,
+                memory_bank,
+                context_feats,
+                batch,
+                src_vocabs,
+                memory_lengths=memory_lengths,
+                src_map=src_map,
+                step=step,
+                batch_offset=beam._batch_offset, tags=tags)
+
+            beam.advance(log_probs, attn)
+            any_beam_is_finished = beam.is_finished.any()
+            if any_beam_is_finished:
+                beam.update_finished()
+                if beam.done:
+                    break
+
+            select_indices = beam.current_origin
+
+            if any_beam_is_finished:
+                # Reorder states.
+                if isinstance(memory_bank, tuple):
+                    memory_bank = tuple(x.index_select(1, select_indices)
+                                        for x in memory_bank)
+                else:
+                    memory_bank = memory_bank.index_select(1, select_indices)
+
+                memory_lengths = memory_lengths.index_select(0, select_indices)
+
+                if src_map is not None:
+                    src_map = src_map.index_select(1, select_indices)
+
+            self.model.decoder.map_state(
+                lambda state, dim: state.index_select(dim, select_indices))
+
+        results["scores"] = beam.scores
+        results["predictions"] = beam.predictions
+        results["attention"] = beam.attention
+        return results
+
+
+    def _decode_and_generate(
+        self,
+        decoder_in,
+        memory_bank,
+        context_feats,
+        batch,
+        src_vocabs,
+        memory_lengths,
+        src_map=None,
+        step=None,
+        batch_offset=None,
+        tags=[]):
+        if self.copy_attn:
+            # Turn any copied words into UNKs.
+            decoder_in = decoder_in.masked_fill(
+                decoder_in.gt(self._tgt_vocab_len - 1), self._tgt_unk_idx
+            )
+
+        # Decoder forward, takes [tgt_len, batch, nfeats] as input
+        # and [src_len, batch, hidden] as memory_bank
+        # in case of inference tgt_len = 1, batch = beam times batch_size
+        # in case of Gold Scoring tgt_len = actual length, batch = 1 batch
+        dec_out, context_outs, dec_attn = self.model.decoder(
+            decoder_in, memory_bank, context_proj, memory_lengths=memory_lengths, step=step
+        )
+
+        # Generator forward.
+        if not self.copy_attn:
+            if "std" in dec_attn:
+                attn = dec_attn["std"]
+            else:
+                attn = None
+            log_probs = self.model.generator(dec_out.squeeze(0))
+            # returns [(batch_size x beam_size) , vocab ] when 1 step
+            # or [ tgt_len, batch_size, vocab ] when full sentence
+        else:
+            attn = dec_attn["copy"]
+            scores = self.model.generator(dec_out.view(-1, dec_out.size(2)),
+                                          attn.view(-1, attn.size(2)),
+                                          src_map, tags)
+            # here we have scores [tgt_lenxbatch, vocab] or [beamxbatch, vocab]
+            if batch_offset is None:
+                scores = scores.view(batch.batch_size, -1, scores.size(-1))
+            else:
+                scores = scores.view(-1, self.beam_size, scores.size(-1))
+            scores = collapse_copy_scores(
+                scores,
+                batch,
+                self._tgt_vocab,
+                src_vocabs,
+                batch_dim=0,
+                batch_offset=batch_offset
+            )
+            scores = scores.view(decoder_in.size(0), -1, scores.size(-1))
+            log_probs = scores.squeeze(0).log()
+            # returns [(batch_size x beam_size) , vocab ] when 1 step
+            # or [ tgt_len, batch_size, vocab ] when full sentence
+        return log_probs, attn
