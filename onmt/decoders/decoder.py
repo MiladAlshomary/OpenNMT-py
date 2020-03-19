68cb50fad071e4663d268a6ec51a6337b68cc576
Milad <milad.alshomary@uni-weimar.de>
2020-03-19 13:01:46 +0100

    updates

--- onmt/decoders/decoder.py
+++ onmt/decoders/decoder.py
@@ -760,7 +760,8 @@ class InputFeedRNNDecoderDoublyAttentive(RNNDecoderBaseDoublyAttentive):
 
               decoder_output = decoder_output2 #self.decoder_output_layer(torch.cat((decoder_output1, decoder_output2), 1))
 
-              attns["std"].append(p_attn)
+              #attns["std"].append(p_attn)
+              attns["std"].append(key_phrase_attn)
               attns["std_key_phrases"].append(key_phrase_attn)
           else:
               decoder_output = rnn_output
